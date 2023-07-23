665c84878042231b678ff8600381742aba3f5412
Natasha2702 <Moyanenatasha@gmail.com>
2023-07-23 20:18:35 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <br />
+        <a href="https://github.com/Natasha2702/week-4-homework/tree/main/src">
+          link to my github
+        </a>
       </header>
     </div>
   );
